7b5ad09d0e7f20d50e32bc81286e77e0ca48e633
Rishabh Mehta <rmehta@ph.iitr.ac.in>
2022-05-20 17:51:05 +0530

    made Util importing cleaner, and cross platform

---
+++ Assignment3/Question1/solution.py
@@ -0,0 +1,17 @@
+'''
+This script provides the solution
+for Question 1 of Assignment 3
+'''
+
+import sys
+from pathlib import Path
+
+# Python imports
+import numpy as np
+
+# Add the Utils Module
+p = Path(__file__).parents[2]
+sys.path.append(str(p))
+
+# Utils module imports
+from Utils.plt_creator import plt_creator
